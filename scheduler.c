344a70edb700e85373e4420ef73e3c032d7eda2e
zihaoxu20010109 <59790644+zihaoxu20010109@users.noreply.github.com>
2022-11-15 05:41:13 -0800

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -48,7 +48,7 @@ int perform_execve(struct PCB* pcb, char* filename, char** pcb_argv){
     }
     int tos, argv, k;
     int argvptr[256];
-    tos = User_Limit- 12 - 1024+36;
+    tos = User_Limit- 12 - 1024;
     int j;
     for(j = 0; j < size; j++){
         tos -= (strlen(pcb_argv[j]) + 1);
