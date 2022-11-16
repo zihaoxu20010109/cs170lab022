55b550c595f4e4ec52e50fab23b515b4359c19ef
zihaoxu20010109 <59790644+zihaoxu20010109@users.noreply.github.com>
2022-11-15 22:28:49 -0800

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -54,9 +54,9 @@ int perform_execve(struct PCB* pcb, char* filename, char** pcb_argv){
     
     int j;
     for(j = 0; j < size; j++){
-        tos -= (strlen(pcb_argv[j]) + 1);
-        while(tos%4 != 0){
-            tos--;
+        //tos -= (strlen(pcb_argv[j]) + 1);
+        //while(tos%4 != 0){
+        //    tos--;
        }
         argvptr[j] = tos;
         strcpy(main_memory+tos+pcb->base, pcb_argv[j]);
