9fa1bd6228a01734ebc8a566a601e4045ae3edc0
zihaoxu20010109 <59790644+zihaoxu20010109@users.noreply.github.com>
2022-11-15 20:32:15 -0800

    Update syscall.c

--- syscall.c
+++ syscall.c
@@ -339,6 +339,7 @@ void myown_exit(void *arg)
         jrb_insert_int(init->children, jval_i(ptr->key), ptr->val);
         struct PCB* temp = (struct PCB*)(ptr->val.v);
         temp->parent = init;
+        print("the true value is %d",init->pid);
     }
     jrb_free_tree(pcb->children);
 
@@ -351,7 +352,7 @@ void myown_exit(void *arg)
             for (int i=0; i<NumTotalRegs; ++i){
                 child->my_registers[i]=0;
             }
-            free(child);
+            //free(child);
         }
     }
     
