2cacd5b52883b7e6ab5c1563bd84e9d8a7f25c66
zihaoxu20010109 <59790644+zihaoxu20010109@users.noreply.github.com>
2022-11-15 21:56:37 -0800

    Update syscall.c

--- syscall.c
+++ syscall.c
@@ -328,7 +328,7 @@ bool empty_memory(int* ptr){
 void myown_exit(void *arg)
 {   
     struct PCB *curr=(struct PCB*)arg;
-    curr->exit_value=pcb->my_registers[5];
+    curr->exit_value=curr->my_registers[5];
     memory_chunk[pcb->mem_int]=0;
     memset(main_memory + pcb->base, 0, pcb->limit);
 
