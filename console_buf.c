a9b2a42a5ea5034e8672d492766fec0e642733e1
zihaoxu20010109 <59790644+zihaoxu20010109@users.noreply.github.com>
2022-12-07 17:38:36 -0800

    Add files via upload

---
+++ console_buf.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "simulator.h"
+#include "console_buf.h"
+#include "kt.h"
+#include "scheduler.h"
+
+kt_sem nelem;
+kt_sem nslots;
+kt_sem consoleWait;
+struct console_buf* buffer;
+
+struct console_buf* init_buff() {
+    struct console_buf* b;
+    b = malloc(sizeof(struct console_buf));
+    b->buff = malloc(256*sizeof(int));
+    b->head = 0;
+    b->tail = 0;
+    b->size = 256;
+    return b;
+}
+
+void initialize_cons_sema() {
+    nelem = make_kt_sem(0);
+    nslots = make_kt_sem(256);
+    consoleWait = make_kt_sem(0);
+}
+
+void cons_to_buff(struct console_buf* buffer) {
+    int finish = 0;
+    while (finish == 0) {
+        P_kt_sem(consoleWait);
+        P_kt_sem(nslots);
+        char c = (char)console_read();
+        V_kt_sem(nelem);
+
+        buffer -> buff[(buffer -> tail)%(buffer->size)] = c;
+        //increment the tail of buffer by 1
+        //The tail and head will be checked by semaphore
+        buffer -> tail = (buffer -> tail + 1)%(buffer->size);
+    }
+    //kt_exit();
+}
---
+++ kos.c
@@ -0,0 +1,28 @@
+/*
+ * kos.c -- starting point for student's os.
+ *
+ */
+#include <stdlib.h>
+
+#include "kt.h"
+#include "simulator_lab2.h"
+#include "simulator.h"
+#include "scheduler.h"
+#include "syscall.h"
+#include "console_buf.h"
+
+void KOS()
+{
+	initialize_sema();
+	initialize_cons_sema();
+
+	
+	initialize_scheduler();
+
+	kt_fork(initialize_user_process, (void*)kos_argv);
+
+	buffer = init_buff();
+	kt_fork((void*)(cons_to_buff), (void*)(buffer));
+	start_timer(10);
+	scheduler();
+}
