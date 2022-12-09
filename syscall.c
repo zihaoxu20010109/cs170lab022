c82a34fd5cae971dd64ed9f5be3c8fc67102f6cd
zihaoxu20010109 <59790644+zihaoxu20010109@users.noreply.github.com>
2022-12-09 15:06:27 -0800

    Update syscall.c

--- syscall.c
+++ syscall.c
@@ -593,7 +593,9 @@ void do_exit(void *arg){
         V_kt_sem(curr->parent->waiters_sem);
         dll_append(curr->parent->waiters, new_jval_v((void*)curr));
     }
+    SYSHalt();
     kt_exit();
+    
 }
 
 void getdtablesize(void *arg){
