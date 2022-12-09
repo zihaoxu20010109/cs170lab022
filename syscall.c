a587e9a90ee1d7fad2b476a3ffed6ee17a6bd509
zihaoxu20010109 <59790644+zihaoxu20010109@users.noreply.github.com>
2022-12-09 00:38:23 -0800

    Update syscall.c

--- syscall.c
+++ syscall.c
@@ -130,7 +130,7 @@ void *do_write(void *arg)
             j++;
             write_count++;
             
-            pcb->fd[file_d_num]->my_pipe->write_head = (pcb->fd[file_d_num]->my_pipe->write_head+1) % (8192-pcb->fd[file_d_num]->my_pipe->writer_in_use);
+            pcb->fd[file_d_num]->my_pipe->write_head = (pcb->fd[file_d_num]->my_pipe->write_head+1) % (8192);
             start_point += 1;
             P_kt_sem(pcb->fd[file_d_num]->my_pipe->space_available);
 
