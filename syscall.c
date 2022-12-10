49f2a1203f8c2cadeab6451fa805c1c71e844494
zihaoxu20010109 <59790644+zihaoxu20010109@users.noreply.github.com>
2022-12-09 16:52:56 -0800

    Update syscall.c

--- syscall.c
+++ syscall.c
@@ -57,7 +57,7 @@ void *do_write(void *arg)
         if (arg1 != 1 && arg1 != 2){   
             syscall_return(pcb, -EBADF);
         }
-	if (arg1 >=2){   
+	if (arg1 >=3){   
             syscall_return(pcb, -EBADF);
         }
         int arg2 = pcb->my_registers[6];
@@ -96,7 +96,7 @@ void *do_write(void *arg)
     
     }else{
 	int arg1 = pcb->my_registers[5];\
-	if (arg1>=2){   
+	if (arg1>=3){   
             syscall_return(pcb, -EBADF);
         }
         int arg2 = pcb->my_registers[6];
